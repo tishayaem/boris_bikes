01b71192de8f9199f68a8f7ed79a5811c3e43c7d
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-19 14:13:53 +0000

    Change solution to the solution provided

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,9 @@ require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  describe 'release_bike' do
-    it { expect(subject.release_bike).to be_an_instance_of Bike}
-    it { expect(Bike.new.working?).to eq true}
+  it ' releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  
   end
 end
