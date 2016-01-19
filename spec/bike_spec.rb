34278220aff4d113314c0d26f41288d34b792f78
Michael Collins <michaelj.collinsuk@gmail.com>
2016-01-19 13:51:39 +0000

    Add working solution to chapter 10

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,4 @@
 require "Bike"
 describe Bike do
   it {is_expected.to respond_to :working?}
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,9 @@
 require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  describe 'release_bike' do
+    it { expect(subject.release_bike).to be_an_instance_of Bike}
+    it { expect(Bike.new.working?).to eq true}
+  end
 end
