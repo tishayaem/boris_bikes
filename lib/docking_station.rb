06b4ea22c183ecaacbcb54355af14d481a60f4a4
Constantin <kostiantynkalinin@gmail.com>
2016-01-21 11:33:34 +0000

    challenges14,15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,28 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
-
+  attr_reader :bikes
+def initialize
+  @bikes = []
+end
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bike
-    @bike = bike
+    fail 'Docking station full' if full?
+
+    @bikes << bike
+
   end
-#dbdjfndf
+
+private
+def full?
+@bikes.count >= 20
+
+end
+def empty?
+@bikes.empty?
+end
 end
